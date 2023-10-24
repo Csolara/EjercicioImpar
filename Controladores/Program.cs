5d5d1be0d993e8e32387dadb38e2f1315dd0a2d6
DESKTOP-5BAJO87\Profesor <csi23-csolara@colegioaltair.net>
2023-10-24 13:32:47 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,26 @@
+﻿using EjercicioImpar.Servicios;
+
+namespace EjercicioImpar
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int n1;
+            MenuInterfaz mi = new MenuImplementacion();
+            n1 = mi.PedirNum();
+
+            if (n1%2==0)
+            {
+                Console.WriteLine("Es par");
+            }
+            else
+            {
+                Console.WriteLine("Es impar");
+            }
+        }
+
+    }
+}
---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioImpar.Servicios
+{
+    class MenuImplementacion : MenuInterfaz
+    {
+        public int PedirNum()
+        {
+        int n1;
+        Console.WriteLine("Introduzca un número");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        return n1;
+        }
+
+    }
+}
